d9b19c509846e09afcb240778c896df82892e933
Yury Staravoitau <yurystaravoitau@gmail.com>
2023-09-04 17:55:18 +0300

    feat: connect redis, express-session, connect-redis  and create simply session

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 import express from "express";
+import session from "express-session";
+import redisStorage from 'connect-redis';
+import redis from 'redis';
+
+const client = redis.createClient();
+client.connect().catch(console.error);
 
 const app = express()
-const port = 3000
+const host = '127.0.0.1';
+const port = 7000;
 
-// add validation subpart
-app.get("/:subpart", (req, res) => {
+app.get('/:subpart', (req, res) => {
+    console.log("subpart: " + req.params["subpart"]);
     res.send("subpart: " + req.params["subpart"])
-})
-
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry cant find that!');
+    res.sendStatus(200);
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry cant find that!');
+app.listen(port, host, function () {
+    console.log(`Server listens http://${host}:${port}`);
 });
+
+app.use(
+    session({
+        store: new redisStorage({
+            host: host,
+            port: 6379,
+            client: client,
+            prefix: "MybitLy",
+            ttl: 3600000,
+        }),
+        secret: 'you secret key',
+        saveUninitialized: true,
+    })
+);
