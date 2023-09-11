2c34c68bc6fc05113682c94c35e15facb7ceb184
Yury Staravoitau <yurystaravoitau@gmail.com>
2023-09-11 03:21:26 +0300

    refactor: refactor db_service

--- src/db_service/db_service.js
+++ src/db_service/db_service.js
@@ -1,18 +1,17 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const dbService = {};
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
+const dbService = {};
 dbService.connect = () => {
 
     const uri = process.env.ATLAS_URI;
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
     async function run() {
         try {
             // Connect the client to the server	(optional starting in v4.7)
